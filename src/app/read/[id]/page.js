d6b7e2ddf33b7350a78eda9e65287964bbad8615
benidene <dlgusehd09@naver.com>
2023-09-11 17:07:11 +0900

    Next.js Practice

---
+++ src/app/read/[id]/page.js
@@ -0,0 +1,16 @@
+export default async function Read(props) {
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_API_URL + `${props.params.id}`,
+    {
+      cache: 'no-cache',
+    }
+  );
+  const topic = await res.json();
+
+  return (
+    <>
+      <h2>{topic.title}</h2>
+      {topic.body}
+    </>
+  );
+}
---
+++ src/app/update/[id]/page.js
@@ -0,0 +1,66 @@
+'use client';
+
+import { useParams, useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+export default function Update() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id;
+  async function refresh() {
+    const resp = await fetch(process.env.NEXT_PUBLIC_API_URL + `/topics/${id}`);
+    const topic = await resp.json();
+    setTitle(topic.title);
+    setBody(topic.body);
+  }
+  useEffect(() => {
+    refresh();
+  }, []);
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        fetch(process.env.NEXT_PUBLIC_API_URL + `topics/` + id, options)
+          .then((res) => res.json())
+          .then((result) => {
+            console.log(result);
+            const lastId = result.id;
+            router.refresh();
+            router.push(`/read/${lastId}`);
+          });
+      }}
+    >
+      <p>
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </p>
+      <p>
+        <textarea
+          name="body"
+          placeholder="body"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+      </p>
+      <p>
+        <input type="submit" value="Update" />
+      </p>
+    </form>
+  );
+}
